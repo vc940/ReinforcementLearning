20b9da726e156e6f41ed602165f23b4f35871c37
Vaibhav <23je0271@gmail.com>
2025-01-18 12:22:12 +0530

    reinforcement learning

--- Lunar_lander(stable_baseline)/run.py
+++ Lunar_lander(stable_baseline)/run.py
@@ -5,7 +5,7 @@ from stable_baselines3 import DQN
 env = gym.make("LunarLander-v3",render_mode = 'human')
 
 # Load the trained model
-model = DQN.load("dqn_lunarlander")
+model = DQN.load("Lunar_lander(stable_baseline)/dqn_lunarlander.zip")
 
 # Evaluate the loaded model
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -13,7 +13,6 @@ from stable_baselines3.common.evaluation import evaluate_policy
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
 print(f"Mean Reward: {mean_reward}, Std Reward: {std_reward}")
 
-# Run the model for a single episode to visualize its performance
 obs, info = env.reset()
 done = False
 total_reward = 0
