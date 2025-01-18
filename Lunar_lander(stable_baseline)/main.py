e183fa84abc380320aa2ca3a8c05f9edaaaa4bf3
Vaibhav <23je0271@gmail.com>
2025-01-18 12:12:22 +0530

    changes

--- Lunar_lander(stable_baseline)/main.py
+++ Lunar_lander(stable_baseline)/main.py
@@ -2,12 +2,10 @@ import gymnasium as gym
 from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 
-# Create the environment
 env = gym.make("LunarLander-v3")
 
-# Initialize the DQN agent
 model = DQN(
-    "MlpPolicy",  # Use a multi-layer perceptron policy
+    "MlpPolicy",  
     env,
     learning_rate=0.001,
     buffer_size=100000,
@@ -22,15 +20,11 @@ model = DQN(
     verbose=1,
 )
 
-# Train the model
 model.learn(total_timesteps=600_000)
 
-# Save the trained model
 model.save("dqn_lunarlander")
 
-# Evaluate the model
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
 print(f"Mean Reward: {mean_reward}, Std Reward: {std_reward}")
 
-# Close the environment
 env.close()
